61e87ae52bac6e27b6aba5fde931b4b7ecbdab64
Clergue Maël <59982953+meyde@users.noreply.github.com>
2025-12-18 15:37:48 +0100

    panel added, ui improved

--- Assets/Scripts/RessourceManager.cs
+++ Assets/Scripts/RessourceManager.cs
@@ -29,12 +29,27 @@ public class RessourceManager
     public int toolsInitialStaticCost;
     public int toolsStaticCost;
     public int toolsBought;
+    public GameObject panelButton;
+    public int panels;
+    public float panelsVoltCost;
+    public float panelsInitialVoltCost;
+    public int panelsWireCost;
+    public int panelsInitialWireCost;
+    public int panelsInitialStaticCost;
+    public int panelsStaticCost;
+    public int panelsToolCost;
+    public int panelsInitialToolCost;
+    public int panelsBought;
+    public int panelsChargeDelay;
     public string doorRessourceCostText;
+    public string doorText;
     public int doorState;
     public int doorVoltCost;
     public int doorWiresCost;
     public int doorStaticCost;
     public int doorToolsCost;
+    public int doorPanelCost;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -42,7 +57,9 @@ public class RessourceManager
         wireButton.SetActive(false);
         staticButton.SetActive(false);
         toolButton.SetActive(false);
+        panelButton.SetActive(false);
         StartCoroutine(StaticsCharge());
+        StartCoroutine(PanelsCharge());
     }
 
     // Update is called once per frame
@@ -57,6 +74,10 @@ public class RessourceManager
     public void staticCharge()
     {
         volts += voltIncrease * 1f / 100f;
+    }
+    public void panelCharge()
+    {
+        volts += voltIncrease * 10;
     }
     public float priceIncrease(float basecost,float price,int bought, float scale)
     {
@@ -96,7 +117,17 @@ public class RessourceManager
             }
         }
     }
-
+    public IEnumerator PanelsCharge()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(panelsChargeDelay);
+            for(int i = 0;i < panels; i++)
+            {
+                panelCharge();
+            }
+        }
+    }
     public void toolsBuy() 
     { 
     if (volts >= toolsVoltCost && wires >= toolsWireCost && statics >= toolsStaticCost) 
@@ -114,30 +145,46 @@ public class RessourceManager
             staticsWireCost = staticsWireCost / 10;
         }
     }
+
+    public void panelsBuy()
+    {
+        if (volts >= panelsVoltCost && wires >= panelsWireCost && statics >= panelsStaticCost && tools >= panelsToolCost)
+        {
+            volts -= panelsVoltCost;
+            wires -= panelsWireCost;
+            statics -= panelsStaticCost;
+            panels += 1;
+            panelsVoltCost = priceIncrease(panelsInitialVoltCost, panelsVoltCost, panelsBought, 4);
+            panelsWireCost = Mathf.RoundToInt(priceIncrease(panelsInitialWireCost, panelsWireCost, panelsBought, 3));
+            panelsStaticCost = Mathf.RoundToInt(priceIncrease(panelsInitialStaticCost, panelsStaticCost, panelsBought, 2));
+            panelsToolCost = Mathf.RoundToInt(priceIncrease(panelsInitialToolCost, panelsToolCost, panelsBought, 1));
+            panelsBought += 1;
+        }
+    }
+
     public void  doorOppening()
     {
         if (doorState ==0 && volts >= doorVoltCost)
         {
             doorState = 1;
-            doorVoltCost = 5000;
-            doorWiresCost = 20;
-            wireCost = 10;
-            wiresBought = 1;
-            doorRessourceCostText = doorVoltCost.ToString() + "Volts and " + doorWiresCost.ToString() + " Wires";
+            doorVoltCost = 500;
+            doorWiresCost = 10;
+            wireCost = initialWireCost;
+            wiresBought = 0;
+            doorRessourceCostText = doorVoltCost.ToString() + " Volts and " + doorWiresCost.ToString() + " Wires";
             volts = 0;
             wireButton.SetActive(true);
         }
         if (doorState == 1 && volts >= doorVoltCost && wires>=doorWiresCost)
         {
             doorState = 2;
-            doorVoltCost = 50000;
-            doorWiresCost = 200;
-            doorStaticCost = 10;
-            wireCost = 10;
-            wiresBought = 1;
-            staticsVoltCost = 5000f;
-            staticsWireCost = 100;
-            staticsBought = 1;
+            doorVoltCost = 5000;
+            doorWiresCost = 100;
+            doorStaticCost = 30;
+            wireCost = initialWireCost;
+            wiresBought = 0;
+            staticsVoltCost = staticsInitialVoltCost;
+            staticsWireCost = staticsInitialWireCost;
             doorRessourceCostText = doorVoltCost.ToString() + " Volts, " + doorWiresCost.ToString() + " Wires and "+ doorStaticCost.ToString() + " Statics";
             volts = 0;
             wires = 0;
@@ -146,24 +193,50 @@ public class RessourceManager
         if (doorState == 2 && volts >= doorVoltCost && wires >= doorWiresCost && statics>=doorStaticCost)
         {
             doorState = 3;
-            doorVoltCost = 50000000;
-            doorWiresCost = 200000;
-            doorStaticCost = 10000;
-            doorToolsCost = 5;
-            wireCost = 10;
-            wiresBought = 1;
-            staticsVoltCost = 500f;
-            staticsWireCost = 10;
-            staticsBought = 1;
-            toolsWireCost = 200;
-            toolsVoltCost = 50000;
-            toolsStaticCost = 100;
-            doorRessourceCostText = doorVoltCost.ToString() + " Vowolts, " + doorWiresCost.ToString() + " Wires, " + doorStaticCost.ToString() + " Statics and " + doorToolsCost.ToString() + "touwuols";
+            doorVoltCost = 50000;
+            doorWiresCost = 1000;
+            doorStaticCost = 300;
+            doorToolsCost = 20;
+            wireCost = initialWireCost;
+            wiresBought = 0;
+            staticsVoltCost = staticsInitialVoltCost;
+            staticsWireCost = staticsInitialWireCost;
+            staticsBought = 0;
+            toolsWireCost = toolsInitialWireCost;
+            toolsVoltCost = toolsInitialVoltCost;
+            toolsStaticCost = toolsInitialStaticCost;
+            doorRessourceCostText = doorVoltCost.ToString() + " Volts, " + doorWiresCost.ToString() + " Wires, " + doorStaticCost.ToString() + " Statics and " + doorToolsCost.ToString() + "tools";
             volts = 0;
             wires = 0;
             statics = 0;
             toolButton.SetActive(true);
-
+        }
+        if (doorState == 3 && volts >= doorVoltCost && wires >= doorWiresCost && statics >= doorStaticCost)
+        {
+            doorState = 4;
+            doorVoltCost = 999999999;
+            doorWiresCost = 99999999;
+            doorStaticCost = 9999999;
+            doorToolsCost = 999999;
+            doorPanelCost = 99999;
+            wireCost = initialWireCost;
+            wiresBought = 0;
+            staticsVoltCost = staticsInitialVoltCost;
+            staticsWireCost = staticsInitialWireCost;
+            staticsBought = 0;
+            toolsWireCost = toolsInitialWireCost;
+            toolsVoltCost = toolsInitialVoltCost;
+            toolsStaticCost = toolsInitialStaticCost;
+            panelsVoltCost = panelsInitialVoltCost;
+            panelsWireCost = panelsInitialWireCost;
+            panelsStaticCost = panelsInitialStaticCost;
+            panelsToolCost = panelsInitialToolCost;
+            doorText = "Final door! to flee, gather : ";
+            doorRessourceCostText = doorVoltCost.ToString() + " Volts, " + doorWiresCost.ToString() + " Wires, " + doorStaticCost.ToString() + " Statics and " + doorToolsCost.ToString() + "tools" + doorPanelCost.ToString() + "panels";
+            volts = 0;
+            wires = 0;
+            statics = 0;
+            panelButton.SetActive(true);
         }
     }
 
--- Assets/Scripts/TextManager.cs
+++ Assets/Scripts/TextManager.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class TextManager : MonoBehaviour
 {
+    public TextMeshProUGUI voltChargeText;
     public TextMeshProUGUI voltAmountText;
     public TextMeshProUGUI wireAmountText;
     public TextMeshProUGUI staticsAmountText;
@@ -12,21 +13,32 @@ public class TextManager : MonoBehaviour
     public TextMeshProUGUI staticsBoostText;
     public TextMeshProUGUI toolsCostText;
     public TextMeshProUGUI toolBoostText;
+    public TextMeshProUGUI toolsAmountText;
+    public TextMeshProUGUI panelsCostText;
+    public TextMeshProUGUI panelBoostText;
+    public TextMeshProUGUI panelAmountText; 
+    public TextMeshProUGUI doorText;
     public TextMeshProUGUI doorRessourceCostText;
     public RessourceManager rm;
 
     void Update()
     {
+        voltChargeText.text = "+ " + rm.voltIncrease.ToString() +" Volts";
         voltAmountText.text = rm.volts.ToString();
         wireAmountText.text = rm.wires.ToString();
         staticsAmountText.text = rm.statics.ToString();
+        toolsAmountText.text = rm.tools.ToString();
+        panelAmountText.text = rm.panels.ToString();
         wiresBoostText.text = "Get an improved wire ";
         wireCostText.text = "Using " +rm.wireCost.ToString() + " Volts";
         staticsBoostText.text = "Build a static charge gathering device";
         staticsCostText.text = "using "+rm.staticsVoltCost.ToString() + " Volts and " + rm.staticsWireCost.ToString() + " Wires";
-        toolBoostText.text = "Build a tool that reduces costs using :";
-        toolsCostText.text = rm.toolsVoltCost.ToString() +" Volts, " + rm.toolsWireCost.ToString() + "Wires and " + rm.toolsStaticCost.ToString() + "Static gatherers";
+        toolBoostText.text = "Build a tool that reduces costs";
+        toolsCostText.text = "using " +rm.toolsVoltCost.ToString() +" Volts, " + rm.toolsWireCost.ToString() + "Wires and " + rm.toolsStaticCost.ToString() + "Static gatherers";
+        panelBoostText.text = "Build a solar pannel";
+        panelsCostText.text = "Using "+ rm.panelsVoltCost.ToString() + " Volts, " + rm.panelsWireCost.ToString() + "Wires, " + rm.panelsStaticCost.ToString() + "Static gatherers and"+ rm.panelsToolCost.ToString() +" Tools";
         doorRessourceCostText.text = rm.doorRessourceCostText;
+        doorText.text = rm.doorText;
 
 
     }
